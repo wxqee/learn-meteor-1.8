b1ed80c397129a3276a81e12c39f5c049b1ea39a
Xiaoqiang Wang <xiaoqiang.wang@storehub.com>
2019-11-01 17:59:38 +0800

    Publish and subscribe

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -42,9 +42,18 @@ class App extends Component {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
     
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+    return filteredTasks.map((task) => {
+      const currentUserId = this.props.currentUser && this.props.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+ 
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
+    });
   }
  
   render() {
@@ -89,6 +98,8 @@ class App extends Component {
 }
 
 export default withTracker(() => {
+  Meteor.subscribe('tasks');
+
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
